b9910806a0835fc7e0d129626e109bd63989a235
gjover06 <georgesamu@yahoo.com>
2020-01-08 14:26:57 -0500

    Exercise 2.3

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HomeController {
+    private Object Model;
+
+    @GetMapping("/personform")
+    public String loadFormPage(Model model) {
+        model.addAttribute("person", new Person());
+        return "personform";
+    }
+
+        @PostMapping("/personform")
+                public String loadFromPage(@ModelAttribute Person person, Model model){
+                    model.addAttribute("person", person);
+                    return "confirmperson";
+        }
+
+
+}
---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Person {
+    private long id;
+    private String name;
+    private int SSN;
+    private int DOB;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(int SSN) {
+        this.SSN = SSN;
+    }
+
+    public int getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(int DOB) {
+        this.DOB = DOB;
+    }
+}
